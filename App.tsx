4c093617a1f33d1e85c6ad1dc01c4050771f5db0
Ankit <codewithankit047@gmail.com>
2023-12-10 17:28:02 +0530

    basics of view,text and btn ui

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text style={{fontSize: 20, textAlign: 'center'}}>Hello world</Text>
-      <Text style={{fontSize: 30}}>Like </Text>
-      <Text style={{fontSize: 30}}>Share </Text>
-      <Text style={{fontSize: 30}}>Comment </Text>
-      <Text style={{fontSize: 30}}>New </Text>
-    </View>
+    <>
+      <View>
+        <Text style={{fontSize: 20, textAlign: 'center'}}>My Dashboard</Text>
+        <Text style={{fontSize: 30}}>Home </Text>
+        <Text style={{fontSize: 30}}>Share </Text>
+        <Text style={{fontSize: 30}}>Contact </Text>
+        <Text style={{fontSize: 30}}>Blog </Text>
+      </View>
+      <View>
+        <Button title="Press here" />
+      </View>
+      <View style={{paddingTop: 20}}>
+        <Button title="Press here" />
+      </View>
+    </>
   );
 };
 
